34447516e49950e6d92b577d4a9ad7880d60a437
Geoffrey Huck <geoffrey@geot.fr>
2024-01-25 11:19:24 +0100

    Fix canonical of homepage. It didn't contain the locale.

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -2,10 +2,46 @@
 
 namespace App\Tests\Controller;
 
-use App\Controller\HomeController;
-use PHPUnit\Framework\TestCase;
+use App\Entity\Language;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class HomeControllerTest extends TestCase
+class HomeControllerTest extends WebTestCase
 {
 
+    public function setupDatabaseFixtures()
+    {
+        $em = self::$container->get('doctrine.orm.entity_manager');
+
+        $languageEN = new Language();
+        $languageEN->setCode('en');
+        $languageEN->setName('English');
+        $em->persist($languageEN);
+
+        $languageFR = new Language();
+        $languageFR->setCode('fr');
+        $languageFR->setName('Français');
+        $em->persist($languageFR);
+
+        $em->flush();
+    }
+
+    public function testHomepageActionCanonical()
+    {
+        $client = static::createClient();
+        $this->setupDatabaseFixtures();
+
+        $client->request('GET', '/en');
+        $this->assertResponseIsSuccessful();
+
+        // Get the first link with rel="canonical".
+        $link = $client->getCrawler()->filter('link[rel="canonical"]')->first();
+        $this->assertEquals('http://localhost/en', $link->attr('href'));
+
+        $client->request('GET', '/fr');
+        $this->assertResponseIsSuccessful();
+
+        // Get the first link with rel="canonical".
+        $link = $client->getCrawler()->filter('link[rel="canonical"]')->first();
+        $this->assertEquals('http://localhost/fr', $link->attr('href'));
+    }
 }
